6e131ce5602f8a508887a36b6f8e92c85b16848c
Toshiki Baba <yz.t0.r33@gmail.com>
2018-11-30 01:54:06 +0900

    addlistのコンポーネント分けた

---
+++ components/organisms/Kanban/AddList.vue
@@ -0,0 +1,69 @@
+<template lang="pug">
+  section.AddList
+    .on(v-if="addList")
+      el-input ＋ リストを追加する
+      el-button.cancel(size="mini" @click="addList = false") キャンセル
+      el-button.add(size="mini") 作成
+    .off(v-else @click="addList = true") ＋ リストを追加する
+</template>
+
+<script>
+export default {
+  name: 'AddList',
+  data() {
+    return {
+      addList: false
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+@import '~assets/styles/_vars';
+@import '~assets/styles/_libs';
+@import '~assets/styles/_type';
+
+.AddList {
+  min-width: 280px;
+  background-color: $color-gray-lighter;
+  padding: 1rem;
+
+  .on {
+    padding-bottom: 0;
+    height: 7rem;
+    display: grid;
+    grid-template-areas: 'input input' 'cancel add';
+  }
+
+  .el-input {
+    grid-area: input;
+  }
+
+  .el-button {
+    width: 6rem;
+    height: 2rem;
+    @include type-xsmall;
+    color: $color-white;
+  }
+
+  .el-button.cancel {
+    grid-area: cancel;
+    background-color: $color-gray;
+  }
+
+  .el-button.add {
+    grid-area: add;
+    justify-self: end;
+    background-color: $color-sky;
+  }
+
+  .off {
+    opacity: 0.5;
+    height: 3rem;
+
+    &:hover {
+      opacity: 0.8;
+    }
+  }
+}
+</style>
--- pages/kanban.vue
+++ pages/kanban.vue
@@ -7,19 +7,18 @@
         :name="l.name"
         :tasks="l.tasks"
       )
-      .add-list.on(v-if="addList")
-        el-input ＋ リストを追加する
-        el-button.cancel(size="mini" @click="addList = false") キャンセル
-        el-button.add(size="mini") 作成
-      .add-list.off(v-else @click="addList = true") ＋ リストを追加する
+      AddList
 </template>
 
 <script>
 import TaskListBox from '~/components/organisms/Kanban/TaskListBox'
+import AddList from '~/components/organisms/Kanban/AddList'
+
 export default {
   name: 'Kanban',
   components: {
-    TaskListBox
+    TaskListBox,
+    AddList
   },
   data() {
     return {
@@ -189,50 +188,6 @@ export default {
 
   .list {
     margin-right: 2rem;
-  }
-
-  .add-list {
-    min-width: 280px;
-    background-color: $color-gray-lighter;
-    padding: 1rem;
-  }
-
-  .on {
-    padding-bottom: 0;
-    height: 7rem;
-    display: grid;
-    grid-template-areas: 'input input' 'cancel add';
-  }
-
-  .el-input {
-    grid-area: input;
-  }
-
-  .el-button {
-    width: 6rem;
-    height: 2rem;
-    @include type-xsmall;
-    color: $color-white;
-  }
-
-  .el-button.cancel {
-    grid-area: cancel;
-    background-color: $color-gray;
-  }
-
-  .el-button.add {
-    grid-area: add;
-    justify-self: end;
-    background-color: $color-sky;
-  }
-
-  .off {
-    opacity: 0.5;
-    height: 3rem;
-
-    &:hover {
-      opacity: 0.8;
-    }
   }
 }
 </style>
