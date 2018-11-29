fe5daf8b0b97a957abb7b91e556ed80c868a3625
Toshiki Baba <yz.t0.r33@gmail.com>
2018-11-30 01:48:15 +0900

    addListの挙動

--- pages/kanban.vue
+++ pages/kanban.vue
@@ -7,7 +7,11 @@
         :name="l.name"
         :tasks="l.tasks"
       )
-      .create-list ＋ リストを追加する
+      .add-list.on(v-if="addList")
+        el-input ＋ リストを追加する
+        el-button.cancel(size="mini" @click="addList = false") キャンセル
+        el-button.add(size="mini") 作成
+      .add-list.off(v-else @click="addList = true") ＋ リストを追加する
 </template>
 
 <script>
@@ -19,6 +23,7 @@ export default {
   },
   data() {
     return {
+      addList: false,
       list: [
         {
           name: 'IceBox',
@@ -186,12 +191,44 @@ export default {
     margin-right: 2rem;
   }
 
-  .create-list {
+  .add-list {
     min-width: 280px;
     background-color: $color-gray-lighter;
+    padding: 1rem;
+  }
+
+  .on {
+    padding-bottom: 0;
+    height: 7rem;
+    display: grid;
+    grid-template-areas: 'input input' 'cancel add';
+  }
+
+  .el-input {
+    grid-area: input;
+  }
+
+  .el-button {
+    width: 6rem;
+    height: 2rem;
+    @include type-xsmall;
+    color: $color-white;
+  }
+
+  .el-button.cancel {
+    grid-area: cancel;
+    background-color: $color-gray;
+  }
+
+  .el-button.add {
+    grid-area: add;
+    justify-self: end;
+    background-color: $color-sky;
+  }
+
+  .off {
     opacity: 0.5;
     height: 3rem;
-    padding: 1rem 0.5rem;
 
     &:hover {
       opacity: 0.8;
