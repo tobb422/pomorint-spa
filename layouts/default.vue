100db36bfa4406ad3ec0e2544b586bc8cdc4d9f6
Toshiki Baba <yz.t0.r33@gmail.com>
2019-03-24 04:22:33 +0900

    ログアウト時は押せない

--- layouts/default.vue
+++ layouts/default.vue
@@ -11,13 +11,13 @@
         placement="bottom"
         width="300"
         trigger="click"
+        v-if="accountImage"
       )
         ul.popover-inner(style="margin: 0.5rem 0.5rem 0")
           li(style="margin-bottom: 0.5rem") #[nuxt-link(to="/mypage") プロフィール]
           el-button(type="text" @click="signout") ログアウト
-        .account-wrap(slot="reference")
-          img.account(:src="accountImage" v-if="accountImage")
-          i.fas.fa-user-circle.no-account(v-else)
+        img.account(:src="accountImage" slot="reference")
+      i.fas.fa-user-circle.no-account(v-else)
     nuxt
     Toast
 </template>
@@ -99,19 +99,17 @@ export default {
     margin-right: 2rem;
   }
 
-  .account-wrap {
+  .account {
     height: 2.5rem;
     width: 2.5rem;
     border-radius: 50%;
-    & > img.account {
-      height: 2.5rem;
-      width: 2.5rem;
-      border-radius: 50%;
-    }
   }
+
   .no-account {
     font-size: 2.5rem;
     color: $color-white;
+    margin-right: 2rem;
+    justify-self: end;
   }
 }
 </style>
