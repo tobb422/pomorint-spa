9148f873bff13c0bfdb06168986153a4309906d0
Toshiki Baba <yz.t0.r33@gmail.com>
2019-05-10 02:35:18 +0900

    ogp

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,9 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      { hid: 'description', name: 'description', content: pkg.description },
+      { hid: 'og:type', property: 'og:type', content: 'website' },
+      { hid: 'og:image', property: 'og:image', content: 'http://pomorint.com/ogp.png' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
