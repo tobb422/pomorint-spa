6d39e11cfd0d5b6b4b7e75422563848f322e7425
Toshiki Baba <yz.t0.r33@gmail.com>
2019-05-12 00:27:08 +0900

    menuの切り替え

--- components/organisms/Default/ProfileIcon.vue
+++ components/organisms/Default/ProfileIcon.vue
@@ -1,21 +1,45 @@
 <template lang="pug">
 .ProfileIcon
-  el-popover(placement="bottom" width="300" trigger="click")
-    ul.popover-inner(style="margin: 0.5rem 0.5rem 0")
-      li(style="margin-bottom: 0.5rem") #[nuxt-link(to="/mypage") プロフィール]
-      el-button(type="text" @click="logout") ログアウト
-    .account(slot="reference")
-      img(v-if="profileImage" :src="profileImage")
-      i.fas.fa-user-circle(v-else)
+  .for-pc(v-if="forPC")
+    el-popover(placement="bottom" width="300" trigger="click")
+      ul.popover-inner(style="margin: 0.5rem 0.5rem 0")
+        li(style="margin-bottom: 0.5rem") #[nuxt-link(to="/mypage") プロフィール]
+        el-button(type="text" @click="logout") ログアウト
+      .account(slot="reference")
+        img(v-if="profileImage" :src="profileImage")
+        i.fas.fa-user-circle(v-else)
+  .for-sp(v-else)
+    el-popover(placement="bottom" width="300" trigger="click")
+      ul.popover-inner(style="margin: 0.5rem 0.5rem 0")
+        li(style="margin-bottom: 0.5rem") #[nuxt-link(to="/mypage") プロフィール]
+        li(style="margin-bottom: 0.5rem") #[nuxt-link(to="/pomodoro") ポモドーロ]
+        li(style="margin-bottom: 0.5rem") #[nuxt-link(to="/kanban") カンバン]
+        el-button(type="text" @click="logout") ログアウト
+      .account(slot="reference")
+        img(v-if="profileImage" :src="profileImage")
+        i.fas.fa-user-circle(v-else)
 </template>
 
 <script>
 import { mapState } from 'vuex'
 
+const Boundary = 800
+
 export default {
+  data() {
+    return {
+      forPC: true
+    }
+  },
   computed: mapState({
     profileImage: state => state.user.image
   }),
+  mounted() {
+    const setForPC = _ => (this.forPC = window.innerWidth >= Boundary)
+
+    setForPC()
+    window.addEventListener('resize', setForPC)
+  },
   methods: {
     async logout() {
       const logout = await this.$store
@@ -40,6 +64,7 @@ export default {
 
 <style lang="scss">
 @import '~assets/styles/_vars';
+@import '~assets/styles/_libs';
 
 .ProfileIcon {
   .account {
